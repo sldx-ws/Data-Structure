d36fda363f44fe3368ac5cefe527a5b8f2e76ed7
sldx-ws <1332644283@qq.com>
2024-02-20 23:46:46 +0800

    添加直接插入排序的时间复杂度注释，实现希尔排序

--- Sort/InsertSort/Sort.c
+++ Sort/InsertSort/Sort.c
@@ -1,5 +1,7 @@
 #include "Sort.h"
 
+// 最坏时间复杂度O(N^2) -- 逆序
+// 最好时间复杂度O(N) -- 顺序有序
 void InsertSort(int* a, int n)
 {
     assert(a);
@@ -24,7 +26,40 @@ void InsertSort(int* a, int n)
         }
     }
 }
-void ShellSort(int* a, int n);
+
+// O(N^1.3)
+void ShellSort(int* a, int n)
+{
+    assert(a);
+
+	int gap = n;
+	while (gap > 1)
+	{
+		gap = gap / 3 + 1;
+
+		// [0,endIndex] 插入 endIndex+gap [0, endIndex+gap]有序  -- 间隔为gap的数据
+		for (int i = 0; i < n - gap; ++i)
+		{
+			int endIndex = i;
+			int tmp = a[endIndex + gap];
+
+			while (endIndex >= 0)
+			{
+				if (a[endIndex] > tmp)
+				{
+					a[endIndex + gap] = a[endIndex];
+					endIndex -= gap;
+				}
+				else
+				{
+					break;
+				}
+			}
+
+			a[endIndex + gap] = tmp;
+		}
+	}
+}
 
 void Print(int* a, int n)
 {
--- Sort/InsertSort/test.c
+++ Sort/InsertSort/test.c
@@ -7,9 +7,17 @@ void test01()
     Print(arr, sizeof(arr) / sizeof(arr[0]));
 }
 
+void test02()
+{
+    int arr[] = { 3, 5, 2, 7, 3, 4, 0 };
+    ShellSort(arr, sizeof(arr) / sizeof(arr[0]));
+    Print(arr, sizeof(arr) / sizeof(arr[0]));
+}
+
 int main()
 {
-    test01(); 
+    //test01(); 
+    test02();
 
     return 0;
 }
