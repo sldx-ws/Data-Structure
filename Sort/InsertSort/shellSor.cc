ba81ed141b9252bf750b55610cf2323797190cff
sldx-ws <1332644283@qq.com>
2024-03-24 00:29:47 +0800

    c++实现希尔排序

---
+++ Sort/InsertSort/shellSor.cc
@@ -0,0 +1,24 @@
+void shellSort(vector<int>& v)
+{
+    int n = v.size();
+
+    int gap = n;
+    while (gap > 1)
+    {
+        gap = gap / 3 + 1;
+
+        for (int i = gap; i < n; ++i)
+        {
+            int tmp = v[i];
+            int j = i;
+
+            while (n >= gap && v[j - gap] > tmp)
+            {
+                v[j] = v[j - gap];
+                j -= gap;
+            }
+
+            v[j] = tmp;
+        }
+    }
+}
