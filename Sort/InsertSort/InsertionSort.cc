af6facdbf64ec2d1ed76b9fac688767fe75502d6
sldx-ws <1332644283@qq.com>
2024-03-23 23:52:38 +0800

    更正错误

--- Sort/InsertSort/InsertionSort.cc
+++ Sort/InsertSort/InsertionSort.cc
@@ -4,7 +4,7 @@ void InsertionSort(vector<int>& v)
     for (int i = 1; i < n; ++i)
     {
         int key = v[i];
-        int j = i - 1
+        int j = i - 1;
         while (j >=0 && v[j] > key)
         {
             v[j + 1] = v[j];
