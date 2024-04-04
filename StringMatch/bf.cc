ecf5bc11c28d7edb14949659fcf1283f7271fea2
sldx-ws <1332644283@qq.com>
2024-04-04 22:55:27 +0800

    实现字符串匹配bf算法

---
+++ StringMatch/bf.cc
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+
+int bf(string str, string sub) {
+    int n = str.length();
+    int m = sub.length();
+
+    for (int i = 0; i <= n - m; ++i) {
+        int j = 0;
+        for (; j < m; ++j) {
+            if (str[i + j] != sub[j])
+                break;
+        }
+
+        if(j == m)
+            return i;
+    }
+
+    return -1;
+}
+
+int main() {
+    string s1("helloworld");
+    string s2("he");
+    cout << bf(s1, s2) << endl; // 0
+
+    return 0;
+}
